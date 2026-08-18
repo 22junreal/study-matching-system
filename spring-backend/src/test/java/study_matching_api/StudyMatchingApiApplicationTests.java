f2c82d6f46c62981ca0af1726a3869fc6092bd24
이의준 <dldmlwns0301@gmail.com>
2026-08-18 16:20:56 +0900

    feat: initialize Spring Boot backend infrastructure

---
+++ spring-backend/src/test/java/study_matching_api/StudyMatchingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package study_matching_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyMatchingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
